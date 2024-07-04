b833202c446b46530eddd50d95e901ab3ac323a6
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-07-04 14:06:32 +0200

    inizio creazione model e migration article

--- Ecommerce_MS/database/seeders/DatabaseSeeder.php
+++ Ecommerce_MS/database/seeders/DatabaseSeeder.php
@@ -4,20 +4,36 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    public $categories = [
+        'Elettronica',
+        'Musica',
+        'Abbigliamento',
+        'Libri',
+        'Giardino',
+        'Casa',
+        'Animali domestici',
+        'Automobili',
+        'Accessori'
+    ];
+
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        foreach ($this->categories as $category){
+            Category::create([
+                'name'=>$category
+            ]);
+        }
     }
 }
