a4fdb3d96d847419805b57fa76ad1ee533aad9bf
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-07-11 17:12:34 +0200

    migliorato responsive schermi medi-grandi

--- Ecommerce_MS/resources/js/script.js
+++ Ecommerce_MS/resources/js/script.js
@@ -1,13 +1,15 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
 let xValue = 0,
- yValue = 0;
+    yValue = 0;
 
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
     parallax_el.forEach((el) => {
-        el.style.transform = `translateX(calc(-50% + ${-xValue}px))  translateY(calc(-50% + ${yValue}px))`;
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px))  translateY(calc(-50% + ${yValue * speedy}px))`;
     });
 });
--- Ecommerce_MS/resources/views/components/main.blade.php
+++ Ecommerce_MS/resources/views/components/main.blade.php
@@ -1,28 +1,30 @@
 <main>
 
-    <img src="{{ asset('img/background.png') }}" alt=""   class="parallax bg_img">
-    <img src="{{ asset('img/fog_7.png') }}" alt=""        class="parallax fog_7">
-    <img src="{{ asset('img/mountain_10.png') }}" alt=""  class="parallax mountain_10">
-    <img src="{{ asset('img/fog_6.png') }}" alt=""        class="parallax fog_6">
-    <img src="{{ asset('img/mountain_9.png') }}" alt=""   class="parallax mountain_9">
-    <img src="{{ asset('img/mountain_8.png') }}" alt=""   class="parallax mountain_8">
-    <img src="{{ asset('img/fog_5.png') }}" alt=""        class="parallax fog_5">
-    <img src="{{ asset('img/mountain_7.png') }}" alt=""   class="parallax mountain_7">
-    <div class="text parallax">
+    <img src="{{ asset('img/background.png') }}" data-speedx="0.001" data-speedy="0.001" alt=""       class="parallax bg_img">
+    <img src="{{ asset('img/fog_7.png') }}" alt="" data-speedx="0.27" data-speedy="0.32"           class="parallax fog_7">
+    <img src="{{ asset('img/mountain_10.png') }}" alt="" data-speedx="0.001" data-speedy="0.001"    class="parallax mountain_10">
+    <img src="{{ asset('img/fog_6.png') }}" alt="" data-speedx="0.25" data-speedy="0.28"           class="parallax fog_6">
+    <img src="{{ asset('img/mountain_9.png') }}" alt="" data-speedx="0.125" data-speedy="0.155"    class="parallax mountain_9">
+    <img src="{{ asset('img/mountain_8.png') }}" alt="" data-speedx="0.1" data-speedy="0.11"       class="parallax mountain_8">
+    <img src="{{ asset('img/fog_5.png') }}" alt="" data-speedx="0.16" data-speedy="0.105"          class="parallax fog_5">
+    <img src="{{ asset('img/mountain_7.png') }}" alt="" data-speedx="0.1" data-speedy="0.1"        class="parallax mountain_7">
+
+     <div class="text parallax">
         <h2>China month</h2>
         <h1>{{env('APP_NAME')}}</h1>
     </div>
-    <img src="{{ asset('img/mountain_6.png') }}" alt=""   class="parallax mountain_6">
-    <img src="{{ asset('img/fog_4.png') }}" alt=""        class="parallax fog_4">
-    <img src="{{ asset('img/mountain_5.png') }}" alt=""   class="parallax mountain_5">
-    <img src="{{ asset('img/fog_3.png') }}" alt=""        class="parallax fog_3">
-    <img src="{{ asset('img/mountain_4.png') }}" alt=""   class="parallax mountain_4">
-    <img src="{{ asset('img/mountain_3.png') }}" alt=""   class="parallax mountain_3">
-    <img src="{{ asset('img/fog_2.png') }}" alt=""        class="parallax fog_2">
-    <img src="{{ asset('img/mountain_2.png') }}" alt=""   class="parallax mountain_2">
-    <img src="{{ asset('img/mountain_1.png') }}" alt=""   class="parallax mountain_1">
-    <img src="{{ asset('img/sun_rays.png') }}" alt=""     class="sun_rays">
+
+    <img src="{{ asset('img/mountain_6.png') }}" alt="" data-speedx="0.065" data-speedy="0.05"      class="parallax mountain_6">
+    <img src="{{ asset('img/fog_4.png') }}" alt="" data-speedx="0.135" data-speedy="0.029"          class="parallax fog_4">
+    <img src="{{ asset('img/mountain_5.png') }}" alt="" data-speedx="0.08" data-speedy="0.020"       class="parallax mountain_5">
+    <img src="{{ asset('img/fog_3.png') }}" alt="" data-speedx="0.8" data-speedy="0.018"           class="parallax fog_3">
+    <img src="{{ asset('img/mountain_4.png') }}" alt="" data-speedx="0.08" data-speedy="0.024"      class="parallax mountain_4">
+    <img src="{{ asset('img/mountain_3.png') }}" alt="" data-speedx="0.04" data-speedy="0.18"       class="parallax mountain_3">
+    <img src="{{ asset('img/fog_2.png') }}" alt="" data-speedx="0.15" data-speedy="0.0115"          class="parallax fog_2">
+    <img src="{{ asset('img/mountain_2.png') }}" alt="" data-speedx="0.0235" data-speedy="0.013"    class="parallax mountain_2">
+    <img src="{{ asset('img/mountain_1.png') }}" alt="" data-speedx="0.027" data-speedy="0.018"     class="parallax mountain_1">
+    <img src="{{ asset('img/sun_rays.png') }}" alt="" class="sun_rays">
     <img src="{{ asset('img/black_shadow.png') }}" alt="" class="black_shadow">
-    <img src="{{ asset('img/fog_1.png') }}" alt=""        class="parallax fog_1">
+    <img src="{{ asset('img/fog_1.png') }}" alt="" data-speedx="0.12" data-speedy="0.01"            class="parallax fog_1">
 
 </main>
