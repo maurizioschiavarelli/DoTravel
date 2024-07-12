659c08a7f96aef6bea127d43c1e3f5dbe7d1e5e2
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-07-12 23:06:35 +0200

    implementata funzionalita logout e sistemato front end form login register

--- Ecommerce_MS/resources/views/components/navbar.blade.php
+++ Ecommerce_MS/resources/views/components/navbar.blade.php
@@ -1,9 +1,14 @@
 <nav>
     <a href="{{route('homepage')}}" class="logo">DoTravel</a>
     <ul>
-        <li><a href="{{route('login')}}">LOGIN</a></li>
-        <li><a href="{{route('register')}}">REGISTER</a></li>
-        <li class="search"><a href=""><i class="fa-solid fa-magnifying-glass"></i></a></li>
-        <li class="dropdown"><a href=""><i class="fa-solid fa-bars"></i></a></li>
+        @auth
+            <li><a href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">LOGOUT</a></li>
+            <form action="{{route('logout')}}" method="post" class="d-none" id="form-logout">@csrf</form>
+        @else
+            <li><a href="{{route('login')}}">LOGIN</a></li>
+            <li><a href="{{route('register')}}">REGISTER</a></li>
+        @endauth
+            <li class="search"><a href=""><i class="fa-solid fa-magnifying-glass"></i></a></li>
+            <li class="dropdown"><a href=""><i class="fa-solid fa-bars"></i></a></li>
     </ul>
 </nav>
